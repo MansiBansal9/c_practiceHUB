5d1366a8f176b7d479ca158c48d67e41ccef95f7
MANSI BANSAL <mansi8elfps@gmail.com>
2026-01-05 12:02:43 +0530

    Add program to calculate string length
    this program calculates the length of user's inputed string mannually without using header function strlen().
    this has done using while loop with the specified condition.

---
+++ arrays/lengthstring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    char str[10];
+    printf("enter the string \n");
+    gets(str);
+    printf("\n");
+    int i=0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    
+    printf("the length of this string is %d",i);
+}
